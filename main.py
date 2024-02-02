2b2a3ce3dc7465bf630597f1d7a081ae4c71c7bb
gittolstov <60322564+gittolstov@users.noreply.github.com>
2024-02-02 16:11:30 +0300

    added f

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 HTML = ""
 with open("templates/day-8.html", "r", encoding="UTF-8") as file:
     HTML = file.read()
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     return HTML
+
 
 @app.route("/index")
 def index():
     return render_template("index.html")
 
+
+@app.route('/day-<num>')
+def day(num):
+    return render_template(f'day-{num}.html')
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        for item in request.form:
+            print(item, request.form[item])
+    return render_template("login.html")
+
+
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
